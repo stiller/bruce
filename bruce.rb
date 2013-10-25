1399ac81595fa091f1116a1a71ed5163cfa229c3
Joachim Nolten <joachim@nolten.org>
2013-10-25 14:39:20 +0200

    Add a simple WeightedBanner.

--- bruce.rb
+++ bruce.rb
@@ -11,12 +11,22 @@ class Banner
 end
 
 class RandomBanner
+  attr_reader :banners
+
   def initialize banner_hash
     @banners = banner_hash.map { |k,v| Banner.new(k,v) }
   end
 
   def pick number
     @banners.sample(number)
+  end
+end
+
+class WeightedBanner < RandomBanner
+  def pick number
+    pick_list = []
+    @banners.each { |banner| banner.weight.times { pick_list << banner } }
+    pick_list.sample(number)
   end
 end
 
@@ -35,5 +45,20 @@ describe RandomBanner do
   it 'picks 11 different banners' do
     random_banners = RandomBanner.new(@banners).pick(11)
     random_banners.map(&:name).uniq.size.must_equal 11
+  end
+
+  describe WeightedBanner do
+    it 'will probably - but not always - pick a banner with a very high weight' do
+      @banners['red'] = 999
+      random_banners = WeightedBanner.new(@banners).pick(2)
+      assert random_banners.find{|rb| rb.name == 'red'}
+    end
+
+    it 'probably will not - but might - pick a banner with a very low weight' do
+      @banners.each { |k,v| v = 99 }
+      @banners['red'] = 1
+      random_banners = WeightedBanner.new(@banners).pick(2)
+      refute random_banners.find{ |rb| rb.name == 'red' }
+    end
   end
 end
