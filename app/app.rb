be73acbf15087b6cfd1239862c37402bb136480e
Joachim Nolten <joachim@nolten.org>
2013-10-27 17:48:35 +0100

    Implement slow, non-cached version.

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,3 @@
-module Bruce
   class App < Padrino::Application
     use ActiveRecord::ConnectionAdapters::ConnectionManagement
     register Padrino::Rendering
@@ -7,8 +6,11 @@ module Bruce
     enable :sessions
 
     get "/" do
-      "Hello World!"
+      # Ok, I did NOT think this through
+      banners = Banner.all.map{|b| {b.url => b.weight}}.reduce(Hash.new, :merge)
+      list = Bruce::ListGenerator.new(Bruce::RandomBanner.new(banners),
+                                      Bruce::WeightedBanner.new(banners),3,7)
+      list.pick(10).first.name
     end
 
   end
-end
