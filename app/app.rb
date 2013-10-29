e0124c90c8a25855db9cf069b7022fb6f7953563
Joachim Nolten <joachim@nolten.org>
2013-10-29 10:45:36 +0100

    Add functional test, Redis cache, refactor cache key.

--- app/app.rb
+++ app/app.rb
@@ -9,7 +9,7 @@
 
 
     get "/" do
-      @list = $redis.get("banners")
+      @list = $redis.get(redis_key)
       if @list
         @list = JSON.parse(@list)
         @list = @list.map{ |banner| Bruce::Banner.new(banner['name'],banner['weight']) }
@@ -17,10 +17,14 @@
         banners = Banner.all.map{|b| {b.url => b.weight}}.reduce(Hash.new, :merge)
         @list = Bruce::ListGenerator.new(Bruce::RandomBanner.new(banners),
                                          Bruce::WeightedBanner.new(banners),3,7).pick(15)
-        $redis.set("banners",@list.to_json)
-        $redis.expire("banners",10)
+        $redis.set(redis_key,@list.to_json)
+        $redis.expire(redis_key,10)
       end
       render :erb, "<img src='<%= @list.sample.name %>'>"
+    end
+
+    def redis_key
+      "banners"
     end
 
   end
---
+++ test/functional/index_test.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
+
+describe 'The Bruce App' do
+  before do
+    Banner.create(url: 'foo', weight: 42)
+  end
+
+  after do
+    Banner.delete_all
+  end
+
+  it 'returns an image tag' do
+    get '/'
+    assert last_response.ok?
+    assert last_response.body.include?('img')
+  end
+end
