f15e7a1dc75a5550d596ec5da6be84d1c9ab0f57
Joachim Nolten <joachim@nolten.org>
2013-11-12 13:06:20 +0100

    Some refactors.

--- app/app.rb
+++ app/app.rb
@@ -10,33 +10,46 @@ module Bruce
 
 
     get "/" do
-      @list = $redis.cache(redis_key,10) do
+      list = $redis.cache(redis_key,10) do
         banners = Banner.all.map{|b| {b.url => b.weight}}.reduce(Hash.new, :merge)
         list = Strategies::ListGenerator.new(Strategies::RandomBanner.new(banners),
                Strategies::WeightedBanner.new(banners),
                3,7).pick(15)
         list.to_json
       end
-      @list = JSON.parse(@list)
-      @urls = @list.map{ |banner| banner['name'] }
-      @value = get_new_value_for("#{request.ip}", @urls)
-      render :erb, "<img src='<%= @value %>'>"
+      list = JSON.parse(list)
+      urls = list.map{ |banner| banner['name'] }
+      @banner_url = fetch_banner_for request_key
+      @banner_url = get_another_value_for(@banner_url, urls)
+      save_banner_for request_key, @banner_url
+      render :erb, "<img src='<%= @banner_url %>'>"
     end
 
     def redis_key
       "banners"
     end
 
-    def get_new_value_for key, collection
+    def request_key
+      "#{request.ip}"
+    end
+
+    def expire_time
+      600
+    end
+
+    def fetch_banner_for key
+      $redis.get(key)
+    end
+
+    def save_banner_for key, banner
+      $redis.set(key,banner)
+      $redis.expire(key,expire_time)
+    end
+
+    def get_another_value_for current_value, collection
       new_value = collection.sample
       return new_value if single_unique_element_in collection
-
-      if (previous_value = $redis.get(key))
-        new_value = collection.reject{|element| element == previous_value}.sample
-      end
-      $redis.set(key,new_value)
-      $redis.expire(key,600)
-      new_value
+      return collection.reject{|element| element == current_value}.sample
     end
 
     def single_unique_element_in collection
--- test/functional/index_test.rb
+++ test/functional/index_test.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 
 describe 'The Bruce App' do
   before do
-    Banner.create(url: 'foo', weight: 42)
+    Banner.create(url: 'foo', weight: low_weight)
   end
 
   after do
@@ -14,4 +14,23 @@ describe 'The Bruce App' do
     assert last_response.ok?
     assert last_response.body.include?('img')
   end
+
+  it 'never returns the same banner twice for at least two banners' do
+    Banner.create(url: 'bar', weight: low_weight)
+    previous = ''
+    4.times do
+      get '/'
+      url = last_response.body.match(/src='(.*)'/)[1]
+      assert url != previous
+      previous = url
+    end
+  end
+end
+
+def high_weight
+  100
+end
+
+def low_weight
+  1
 end
