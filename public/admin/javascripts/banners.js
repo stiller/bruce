7fb33d4767bd614d2a953f7df36f9947eb1c7d17
Joachim Nolten <joachim@nolten.org>
2013-11-17 16:08:37 +0100

    Dynamically add Banner fields.

---
+++ public/admin/javascripts/banners.js
@@ -0,0 +1,7 @@
+$('form').on('click', '.add_fields', function(event) {
+  var regexp, time;
+  time = new Date().getTime();
+  regexp = new RegExp($(this).data('id'), 'g');
+  $(this).before($(this).data('fields').replace(regexp, time));
+  return event.preventDefault();
+});
