397ba595e5fb40ff9affeed844b178bfcd0d11cc
Joachim Nolten <joachim@nolten.org>
2013-11-18 07:59:24 +0100

    Add Strategy selector for Campaign.

--- models/campaign.rb
+++ models/campaign.rb
@@ -2,4 +2,8 @@ class Campaign < ActiveRecord::Base
   has_many :selections
   has_many :banners, through: :selections
   accepts_nested_attributes_for :selections, allow_destroy: true
+
+  def self.strategies
+    %w(Random Weighted)
+  end
 end
